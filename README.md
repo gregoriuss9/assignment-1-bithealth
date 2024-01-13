37dfb0787a51b2936007ca7af2dce34a5f488122
Gregorius <kamandanug@gmail.com>
2024-01-13 14:43:28 +0700

    add photos, edit readme, and responsive for extrasmall screen

--- README.md
+++ README.md
@@ -15,3 +15,18 @@ In general, the web structure consist of these sections:
 |   └── IDENTITY
 │   └── STATUS
 ```
+
+## Website view 
+
+1. Mobile view, using `.xs` breakpoint 
+<p align="center">
+    <img src='images/phones.png'/>
+
+2. Tablet view, using `.sm` breakpoint 
+<p align="center">
+    <img src='images/tablet.png'/>
+
+3. Small laptop and desktop view, using `.md` breakpoint 
+<p align="center">
+    <img src='images/desktop.png'/>
+
---
+++ js/script.js
@@ -0,0 +1,17 @@
+let x = document.getElementsByClassName('kelas1')
+console.log(x)
+
+function myFunction(){
+//     let formName = document.getElementById('exampleInputName').value;
+//     let formPosition = document.getElementById('exampleInputPosition').value;
+//     let formAvailbility = document.getElementById('exampleInputAvailbility').value;
+//     let formUsia = document.getElementById('exampleInputUsia').value;
+//     let formLokasi = document.getElementById('exampleInputLokasi').value;
+//     let formPengalaman = document.getElementById('exampleInputPengalaman').value;
+//     let formEmail = document.getElementById('exampleInputEmail').value;
+
+
+//     document.getElementById('full-name').innerHTML = formName;
+// let value = document.getElementsByClassName('kelas1').value;
+// document.getElementsByClassName('kelas1-value').innerHTML = value;
+}
